1412c9bdc4a2d7c7456e1d932ccbbc1f5ab356f3
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2026-06-13 18:58:02 +0900

    Discord.js v14を使用してコマンドをスラッシュコマンドに移行し、環境変数から設定を読み込むように変更

--- index.js
+++ index.js
@@ -1,103 +1,152 @@
-const config = require("./config.json");
-const discord = require("discord.js");
-const client = new discord.Client();
-const child_process = require("child_process");
+const {
+	Client,
+	GatewayIntentBits,
+	SlashCommandBuilder,
+	REST,
+	Routes,
+	MessageFlags,
+} = require('discord.js');
+const child_process = require('child_process');
+require('dotenv').config({ quiet: true });
 
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds],
+});
+
+//////////////////////////////////////////////////////////////
+// load config
+const token = process.env.token;
+const ownerID = process.env.ownerID;
+const usageGuildID = process.env.usageGuildID;
+const ipAddress = process.env.ipAddress;
+const macAddress = process.env.macAddress;
+
+//////////////////////////////////////////////////////////////
+// Command definition
+client.commands = [];
+const wake = new SlashCommandBuilder()
+	.setName('wake')
+	.setDescription('PCを起動します。');
+const ping = new SlashCommandBuilder()
+	.setName('ping')
+	.setDescription('Pingを測定します。');
+const uptime = new SlashCommandBuilder()
+	.setName('uptime')
+	.setDescription('Botの稼働時間を表示します。');
+client.commands.push(wake);
+client.commands.push(ping);
+client.commands.push(uptime);
+
+//////////////////////////////////////////////////////////////
 // Online status
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag} on ${Date()}!`);
+client.on('clientReady', () => {
+	const rest = new REST({ version: '10' }).setToken(token);
+	async () => {
+		try {
+			await rest.put(Routes.applicationGuildCommands(usageGuildID), {
+				body: await client.commands,
+			});
+			console.log('スラッシュコマンドの再読み込みに成功しました。');
+		} catch (err) {
+			console.log(
+				`❌ スラッシュコマンドの再読み込み時にエラーが発生しました：\n${err}`,
+			);
+		}
+	};
+
+	console.log(`Logged in as ${client.user.tag} on ${Date()}!`);
 });
 
-// Wake command
-client.on("message", (msg) => {
-  if (msg.author.id === config.ownerID && msg.content === "wake") {
-    child_process.exec(
-      "wakeonlan -i ${config.ipAddress} ${config.macAddress}",
-      (err, stdout) => {
-        if (err) {
-          msg.channel.send(err);
-        } else {
-          msg.channel.send("woken ✅");
-        }
-      }
-    );
-  }
+//////////////////////////////////////////////////////////////
+// Command handling
+Client.on('interactionCreate', async (interaction) => {
+	try {
+		if (
+			!interaction.inGuild() &&
+			interaction.author.id === ownerID &&
+			interaction.guilds.id === usageGuildID
+		) {
+			await interaction.reply({
+				content:
+					'このBOTは、特定のサーバーの特定のユーザーのみが利用できます。不明な点やご質問などは、[サポートサーバー](https://discord.gg/uYYaVRuUuJ)からお問い合わせください。',
+				flags: MessageFlags.Ephemeral,
+			});
+			return;
+		} else {
+			if (interaction.isApplicationCommand()) {
+				const { commandName } = interaction;
+
+				switch (commandName) {
+					case 'wake': {
+						// wake command
+						child_process.exec(
+							`wakeonlan -i ${ipAddress} ${macAddress}`,
+							(err, stdout) => {
+								if (err) {
+									interaction.reply({
+										content: '❌ PCの起動中にエラーが発生しました。',
+										flags: MessageFlags.Ephemeral,
+									});
+								} else {
+									interaction.reply({
+										content: '✅ PCの起動コマンドを送信しました。',
+										flags: MessageFlags.Ephemeral,
+									});
+								}
+							},
+						);
+						break;
+					}
+					case 'ping': {
+						// ping command
+						const latency = Date.now() - interaction.createdTimestamp;
+						interaction.reply({
+							content: `🏓 Latency is \`${latency}ms\`. \nAPI Latency is \`${Math.round(
+								client.ws.ping,
+							)}ms\`.`,
+							flags: MessageFlags.Ephemeral,
+						});
+					}
+					case 'uptime': {
+						const hours = Math.round(client.uptime / 1000 / 3600);
+						interaction.reply({
+							content: `Uptime is \`${hours}\` hours.`,
+							flags: MessageFlags.Ephemeral,
+						});
+					}
+					case 'status': {
+						child_process.exec(
+							`ping ${ipAddress} -c 1`,
+							(err, stdout) => {
+								if (err) {
+									await interaction.reply({
+                    content: 'PC is offline.',
+                    flags: MessageFlags.Ephemeral,
+                  });
+                  return;
+								} else {
+									const response = stdout.trim();
+									if (response) {
+										await interaction.reply({
+											content: response,
+											flags: MessageFlags.Ephemeral,
+										});
+									} else {
+										await interaction.reply({
+											content: 'No response. PC is offline.',
+											flags: MessageFlags.Ephemeral,
+										});
+									}
+								}
+							},
+						);
+					}
+				}
+			}
+		}
+	} catch (err) {
+		console.error(`Error handling command: ${err}`);
+	}
 });
 
-// Ping and uptime command
-client.on("message", (message) => {
-  if (message.content === "ping") {
-    const latency = Date.now() - message.createdTimestamp;
-    message.channel.send(
-      `🏓 Latency is \`${latency}ms\`. \nAPI Latency is \`${Math.round(
-        client.ws.ping
-      )}ms\`.`
-    );
-  } else if (message.content === "uptime") {
-    const hours = Math.round(client.uptime / 1000 / 3600);
-    message.channel.send(`Uptime is \`${hours}\` hours.`);
-  } else if (message.author.id === config.ownerID) {
-    if (message.content.startsWith("eval ")) {
-      evaluate(message);
-    } else if (message.content.startsWith("exec ")) {
-      execute(message);
-    } else if (message.content === "status") {
-      Status(message);
-    }
-  }
-});
-
-// Evaluate command
-function evaluate(message) {
-  const code = message.content.slice("eval ".length);
-  try {
-    let evaled = eval(code);
-    if (typeof evaled !== "string") {
-      evaled = require("util").inspect(evaled);
-    }
-    message.channel.send(`\`\`\`\n${cleanText(evaled)}\`\`\``);
-  } catch (err) {
-    message.channel.send(`\`ERROR\` \`\`\`\n${cleanText(err)}\n\`\`\``);
-  }
-}
-
-// Execute command
-function execute(message) {
-  const command = message.content.slice("exec ".length);
-  child_process.exec(command, (err, stdout, stderr) => {
-    if (err) {
-      message.channel.send(err);
-    } else {
-      message.channel.send("```" + stdout + stderr + "```");
-    }
-  });
-}
-
-// Status command
-function Status(message) {
-  child_process.exec("ping " + config.ipAddress + " -c 1", (err, stdout) => {
-    if (err) {
-      message.channel.send(err);
-    } else {
-      const response = stdout.trim();
-      if (response) {
-        message.channel.send(response);
-      } else {
-        message.channel.send("No response. PC is offline.");
-      }
-    }
-  });
-}
-
-
-// Clean text for discord markdown
-function cleanText(text) {
-  if (typeof text === "string") {
-    return text
-      .replace(/`/g, "`" + String.fromCharCode(8203))
-      .replace(/@/g, "@" + String.fromCharCode(8203));
-  }
-  return text;
-}
-
-client.login(config.token);
+client.login(token);
