6c1b34fc35ae7815f0e82e4a93c3711273e326d3
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2026-06-14 10:46:35 +0900

    wakeonlanコマンドの引数を修正し、IPアドレスを省略

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ client.on('interactionCreate', async (interaction) => {
 						flags: MessageFlags.Ephemeral,
 					});
 					child_process.exec(
-						`wakeonlan -i ${ipAddress} ${macAddress}`,
+						`wakeonlan ${macAddress}`,
 						async (err) => {
 							if (err) {
 								await interaction.editReply({
